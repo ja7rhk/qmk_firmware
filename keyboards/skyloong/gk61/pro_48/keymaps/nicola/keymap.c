31e9ef723172191d8e4edf364cd3a886fd8f68c6
Sumio Koseki <koseki.sumio@nifty.com>
2024-05-02 23:38:35 +0900

    changed cursor key

--- keyboards/skyloong/gk61/pro_48/keymaps/nicola/keymap.c
+++ keyboards/skyloong/gk61/pro_48/keymaps/nicola/keymap.c
@@ -34,27 +34,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
     [_QWERTY] = LAYOUT_all(
-        KC_ESC,       KC_1,     KC_2,     KC_3,     KC_4,     KC_5,        KC_6,     KC_7,    KC_8,     KC_9,      KC_0,      KC_MINS,  KC_EQL,   KC_BSPC,
-        KC_TAB,       KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,        KC_Y,     KC_U,    KC_I,     KC_O,      KC_P,      KC_LBRC,  KC_RBRC,  KC_BSLS,
-        KC_CAPS_LOCK, KC_A,     KC_S,     KC_D,     KC_F,     KC_G,        KC_H,     KC_J,    KC_K,     KC_L,      KC_SCLN,   KC_QUOT,            KC_ENT,
-        KC_LSFT,      KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,        KC_N,     KC_M,    KC_COMM,  KC_DOT,    KC_SLSH,             KC_RSFT,
-        KC_LCTL,      KC_LGUI,  KC_LALT,            KC_F14,   _______,     KC_F15,            KC_SPC,   KC_RALT,   KC_APP,    KC_RCTL,            MO(_FUNC)
+        KC_ESC,       KC_1,     KC_2,     KC_3,     KC_4,      KC_5,      KC_6,     KC_7,     KC_8,     KC_9,      KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
+        KC_TAB,       KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,      KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
+        KC_CAPS_LOCK, KC_A,     KC_S,     KC_D,     KC_F,      KC_G,      KC_H,     KC_J,     KC_K,     KC_L,      KC_SCLN,  KC_QUOT,            KC_ENT,
+        KC_LSFT,      KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,      KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,            KC_UP,
+        KC_LCTL,      KC_LGUI,  KC_LALT,            KC_F14,    _______,   KC_F15,             KC_SPC,   MO(_FUNC), KC_LEFT,  KC_DOWN,            KC_RIGHT
     ),
 
     [_NICOLA] = LAYOUT_all(
-        KC_ESC,       NG_1,     NG_2,     NG_3,     NG_4,     NG_5,        NG_6,     NG_7,    NG_8,     NG_9,      NG_0,     NG_MINS,  NG_EQL,   KC_BSPC,
-        KC_TAB,       NG_Q,     NG_W,     NG_E,     NG_R,     NG_T,        NG_Y,     NG_U,    NG_I,     NG_O,      NG_P,     NG_LBRC,  NG_RBRC,  NG_BSLS,
-        KC_CAPS_LOCK, NG_A,     NG_S,     NG_D,     NG_F,     NG_G,        NG_H,     NG_J,    NG_K,     NG_L,      NG_SCLN,  KC_BSPC,            KC_ENT,
-        KC_LSFT,      NG_Z,     NG_X,     NG_C,     NG_V,     NG_B,        NG_N,     NG_M,    NG_COMM,  NG_DOT,    NG_SLSH,            KC_RSFT,
-        KC_LCTL,      KC_LGUI,  KC_LALT,            NG_SHFTL, _______,     KC_F15,            NG_SHFTR, KC_RALT,   KC_APP,    KC_RCTL,            MO(_FUNC)
+        KC_ESC,       NG_1,     NG_2,     NG_3,     NG_4,      NG_5,      NG_6,     NG_7,     NG_8,     NG_9,      NG_0,     NG_MINS,  NG_EQL,   KC_BSPC,
+        KC_TAB,       NG_Q,     NG_W,     NG_E,     NG_R,      NG_T,      NG_Y,     NG_U,     NG_I,     NG_O,      NG_P,     NG_LBRC,  NG_RBRC,  NG_BSLS,
+        KC_CAPS_LOCK, NG_A,     NG_S,     NG_D,     NG_F,      NG_G,      NG_H,     NG_J,     NG_K,     NG_L,      NG_SCLN,  KC_BSPC,            KC_ENT,
+        KC_LSFT,      NG_Z,     NG_X,     NG_C,     NG_V,      NG_B,      NG_N,     NG_M,     NG_COMM,  NG_DOT,    NG_SLSH,            KC_UP,
+        KC_LCTL,      KC_LGUI,  KC_LALT,            NG_SHFTL,  _______,   KC_F15,             NG_SHFTR, MO(_FUNC), KC_LEFT,  KC_DOWN,            KC_RIGHT
     ),
 
     [_FUNC] = LAYOUT_all(
-        KC_GRV,       KC_F1,    KC_F2,    KC_F3,    KC_F4,     KC_F5,      KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,   KC_F11,   KC_F12,   _______,
-        _______,      RGB_TOG,  _______,  _______,  _______,   _______,    _______,  KC_PSCR,  _______,  _______,   _______,  _______,  _______,  _______,
-        _______,      _______,  _______,  _______,  _______,   _______,    _______,  _______,  _______,  KC_PGUP,   _______,  _______,            _______,
-        _______,      _______,  _______,  _______,  _______,   _______,    _______,  _______,  _______,  KC_PGDN,   KC_UP,              _______,
-        _______,      _______,  _______,            KC_DEL,    _______,    _______,            KC_SPC,   KC_LEFT,   KC_DOWN,  KC_RIGHT,           _______
+        KC_GRV,       KC_F1,    KC_F2,    KC_F3,    KC_F4,     KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,   KC_F11,   KC_F12,   _______,
+        _______,      RGB_TOG,  _______,  _______,  _______,   _______,   _______,  KC_PSCR,  _______,  _______,   _______,  _______,  _______,  _______,
+        _______,      _______,  _______,  _______,  _______,   _______,   _______,  _______,  _______,  KC_PGUP,   _______,  _______,            _______,
+        _______,      _______,  _______,  _______,  _______,   _______,   _______,  _______,  _______,  KC_PGDN,   _______,            KC_RSFT,
+        _______,      _______,  _______,            KC_DEL,    _______,   _______,            KC_F15,   _______,   KC_RALT,  KC_APP,             KC_RCTL
     )
 };
 
